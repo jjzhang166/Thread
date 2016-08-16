c3c3946cfe7d318c1a2a1826494d570ecbbbf359
litz-a <litz-a@glodon.com>
2016-08-16 21:07:57 +0800

    rich demo

--- ThreadSync/ThreadSync_Lock_Mutex/ThreadSync_Lock_Mutex.cpp
+++ ThreadSync/ThreadSync_Lock_Mutex/ThreadSync_Lock_Mutex.cpp
@@ -21,6 +21,31 @@ void func()
     g_lock.unlock();
 }
 
+int global_counter = 0;
+std::mutex counter_mutex;
+
+void five_thread_fn() {
+    for (int i = 0; i < 5; i++) {
+        counter_mutex.lock();
+        global_counter++;
+        counter_mutex.unlock();
+        std::cout << "Updated from five_thread" << std::endl;
+        std::this_thread::sleep_for(std::chrono::seconds(5));
+    }
+    //When this thread finishes we wait for it to join
+}
+
+void ten_thread_fn() {
+    for (int i = 0; i < 10; i++) {
+        counter_mutex.lock();
+        global_counter++;
+        counter_mutex.unlock();
+        std::cout << "Updated from ten_thread" << std::endl;
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+    }
+    //When this thread finishes we wait for it to join
+}
+
 int main()
 {
     srand((unsigned int)time(0));
@@ -29,9 +54,24 @@ int main()
     std::thread t2(func);
     std::thread t3(func);
 
-    t1.join();
+    t1.join();// wait for t1 to finish(block until func() ends)
     t2.join();
     t3.join();
+
+
+    std::cout << "starting thread ten..." << std::endl;
+    std::thread ten_thread(ten_thread_fn);
+
+    std::cout << "Running ten thread" << std::endl;
+    std::thread five_thread(five_thread_fn);
+
+
+    ten_thread.join();
+    std::cout << "Ten Thread is done." << std::endl;
+    five_thread.join();
+    std::cout << "Five Thread is done." << std::endl;
+
+    int flag = getchar();
 
     return 0;
 }
