cbef8682a53926785e85d8e8f3d87251a82542a7
Bruce Lee <ltz121320@163.com>
2016-09-17 10:49:47 +0800

    format code

--- C__Multithreading_Cookbook/9790OS_Code/9790OS_Chapter_2/ProcessDemo/ProcessDemo.cpp
+++ C__Multithreading_Cookbook/9790OS_Code/9790OS_Chapter_2/ProcessDemo/ProcessDemo.cpp
@@ -6,25 +6,32 @@ using namespace std;
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	STARTUPINFO startupInfo = { 0 };
-	PROCESS_INFORMATION processInformation = { 0 };
+    STARTUPINFO startupInfo = { 0 };
+    PROCESS_INFORMATION processInformation = { 0 };
 
-	BOOL bSuccess = CreateProcess(
-		TEXT("C:\\Windows\\notepad.exe"), NULL, NULL,
-		NULL, FALSE, NULL, NULL, NULL, &startupInfo,
-		&processInformation);
+    BOOL bSuccess = CreateProcess(
+        TEXT("C:\\Windows\\notepad.exe"),
+        NULL,
+        NULL,
+        NULL,
+        FALSE,
+        NULL,
+        NULL,
+        NULL,
+        &startupInfo,
+        &processInformation);
 
-	if (bSuccess)
-	{
-		cout << "Process started." << endl
-			<< "Process ID:\t"
-			<< processInformation.dwProcessId << endl;
-	}
-	else
-	{
-		cout << "Cannot start process!" << endl
-			<< "Error code:\t" << GetLastError() << endl;
-	}
+    if (bSuccess)
+    {
+        cout << "Process started." << endl
+            << "Process ID:\t"
+            << processInformation.dwProcessId << endl;
+    }
+    else
+    {
+        cout << "Cannot start process!" << endl
+            << "Error code:\t" << GetLastError() << endl;
+    }
 
-	return system("pause");
+    return system("pause");
 }
